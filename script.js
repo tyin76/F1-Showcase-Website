acc5f1a998b4c1ea40b86dd049f403e676fdb594
terenceyin <tyin05@student.ubc.ca>
2024-02-27 16:25:25 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function today_date() {
+    let d = new Date();
+    alert("HELLO's date is " + d);
+
+    for (i=0; i < 10; i++) {
+        console.log("GOODBYE");
+    }
+
+    console.log("OH YAAA!");
+}
